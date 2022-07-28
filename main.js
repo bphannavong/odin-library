32095c966c2521ef5c8a0b5f0021d3b9e9850142
Billy Phannavong <billy.phannavong@gmail.com>
2022-07-27 19:18:00 -0700

    Add some function ideas

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.read = Boolean(read);
     this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${read}.` 
+        if (this.read) {
+            return `${title} by ${author}, ${pages} pages, read.` 
+        } else {
+            return `${title} by ${author}, ${pages} pages, not read yet.` 
+        }
+        
     }
-} 
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function displayBooks() {
+    // loop through array and display book on screen
+}
