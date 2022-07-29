945b67408fad2791963e350e22ed0bdcc2cfd507
Billy Phannavong <billy.phannavong@gmail.com>
2022-07-29 13:19:42 -0700

    Finish logic for displayCards

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-let myLibrary = [];
+let myLibrary = [new Book('Moby Dick', 'Steve Jobs', 123, true)];
 
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = Boolean(read);
-    this.info = function() {
-        if (this.read) {
-            return `${title} by ${author}, ${pages} pages, read.` 
-        } else {
-            return `${title} by ${author}, ${pages} pages, not read yet.` 
-        }
-        
+    this.read = Boolean(read);   
+}
+
+Book.prototype.info = function() {
+    if (this.read) {
+        return `${title} by ${author}, ${pages} pages, read.` 
+    } else {
+        return `${title} by ${author}, ${pages} pages, not read yet.` 
     }
 }
 
@@ -24,34 +24,37 @@ function addBookToLibrary() {
     const book = new Book(title, author, pages, read);
     myLibrary.push(book);
 }
-
+const display = document.querySelector('.display');
 function displayBooks() {
     // loop through array and display book on screen
-     for (const book in myLibrary) {
+     for (const book of myLibrary) {
         //create div and add class card to it
         const newCard = document.createElement('div');
-        const titleDiv = document.createElement('div');
-        const authorDiv = document.createElement('div');
-        const pagesDiv = document.createElement('div');
-        const readDiv = document.createElement('div');
+        newCard.classList.add('card');
 
-        newCard.classList.add('newCard');
-
-        titleDiv.classList.add('title');
-        authorDiv.classList.add('author');
-        pagesDiv.classList.add('pages');
-        readDiv.classList.add('read');
-
-        newCard.appendChild(titleDiv);
-        newCard.appendChild(authorDiv);
-        newCard.appendChild(pagesDiv);
-        newCard.appendChild(readDiv);
+        for (const prop in book) {
+            const newDiv = document.createElement('div');
+            if (prop === 'read') {
+                const newText = document.createTextNode('Read? ');
+                const newContent = document.createElement('INPUT');
+                newContent.setAttribute('type', 'checkbox');
+                newDiv.append(newText);
+                newDiv.append(newContent);
+                newCard.append(newDiv); 
+                break;
+            } else {
+                const newContent = document.createTextNode(book[prop]);
+                newDiv.append(newContent);
+                newCard.append(newDiv);
+            }
+            
+        }
         //display book as a card or table
         display.appendChild(newCard);
      }
 }
 
-const display = document.querySelector('.display');
+
 
 const addBook = document.querySelector('button');
 addBook.addEventListener('click', addBookToLibrary);
