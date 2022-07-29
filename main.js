6c3f67445b41a8a003aaa624f11f55c687d33b82
Billy Phannavong <billy.phannavong@gmail.com>
2022-07-29 12:20:11 -0700

    Add basic skeleton and addBookToLibrary

--- main.js
+++ main.js
@@ -15,10 +15,23 @@ function Book(title, author, pages, read) {
     }
 }
 
-function addBookToLibrary(book) {
+function addBookToLibrary() {
+    const title = prompt('Title?', );
+    const author = prompt('Author?', );
+    const pages = prompt('Pages?', );
+    const read = prompt('Read? True or false?', );
+
+    const book = new Book(title, author, pages, read);
     myLibrary.push(book);
 }
 
 function displayBooks() {
     // loop through array and display book on screen
+     for (const book in myLibrary) {
+        //create div and add class card to it
+        //display book as a card or table
+     }
 }
+
+const addBook = document.querySelector('button');
+addBook.addEventListener('click', addBookToLibrary);
